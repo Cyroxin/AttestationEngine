54374978a25856c17c614f72104f47103b12bd30
KlenJavor <patricie.suppala@gmail.com>
2021-11-15 23:07:27 +0200

    test ci

--- apps/mobileattester/app/src/main/java/com/example/mobileattester/ui/pages/More.kt
+++ apps/mobileattester/app/src/main/java/com/example/mobileattester/ui/pages/More.kt
@@ -1,13 +1,40 @@
 package com.example.mobileattester.ui.pages
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 @Preview
-fun More(navController: NavController? = null)
-{
-    Text("More")
+fun More(navController: NavController? = null) {
+
+    MaterialTheme {
+        Button(
+            onClick = { /* ... */ },
+            // Uses ButtonDefaults.ContentPadding by default
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 12.dp,
+                end = 20.dp,
+                bottom = 12.dp
+            )
+        ) {
+            // Inner content including an icon and a text label
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Favorite",
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Like")
+        }
+    }
 }
